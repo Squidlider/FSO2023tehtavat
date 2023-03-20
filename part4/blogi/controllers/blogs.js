aa40f77777ef6d7d4793373467d5160a25076655
Aleksi <aleksi.koskiaho@gmail.com>
2023-03-20 08:55:42 +0200

    4.17. - 4.19.

--- part4/blogi/controllers/blogs.js
+++ part4/blogi/controllers/blogs.js
@@ -1,14 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-
   const { title, author, url, likes } = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!title || !url) {
     return response.status(400).json({ error: 'Title and URL are required' })
@@ -19,9 +35,12 @@ blogsRouter.post('/', async (request, response) => {
     author,
     url,
     likes: likes || 0,
+    user: user._id
   })
 
-  await blog.save()
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(blog)
 })
 
